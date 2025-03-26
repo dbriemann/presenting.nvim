d5ae63aae92fd4d0e0a5563c2105bfee3edc924d
David Linus Briemann <david@briemann.com>
2025-03-26 21:12:50 +0100

    add steps and some navigation functions

---
+++ example_presentation.md
@@ -0,0 +1,38 @@
+# Slide 1
+
+A level 1 markdown header marks the beginning of a new slide.
+Press `n` to advance to the next slide.
+
+## Step 1
+
+Oops. This is still the same slide. A level 2 markdown header marks the beginning of a presentation step.
+By pressing `n` you advance and by pressing `p` you step back. Play around.
+
+## Step 2
+
+Other levels of markdown headers do not have an effect on the presentation.
+
+### A Sub Header
+
+E.g. this level 3 header is just shown on the same step.
+
+## Step 3
+
+Just another step.
+
+## Step 4
+
+The last step of this slide.
+
+# Slide 2
+
+The second slide of this impressive presentation.
+
+## Step 1
+
+You can also go to the beginnig of a slide by pressing TODO or to the end by pressing TODO.
+
+## Step 2
+
+To go to the beginning of a presentation press `f` and to the end by pressing `l`.
+
--- lua/presenting/init.lua
+++ lua/presenting/init.lua
@@ -22,7 +22,7 @@ Presenting.setup = function(config)
   config = H.setup_config(config)
   H.apply_config(config)
 
-  vim.api.nvim_create_user_command("Presenting", Presenting.toggle, {})
+  vim.api.nvim_create_user_command("Presenting", Presenting.toggle, { nargs = "*" })
   vim.api.nvim_create_user_command("PresentingDevMode", Presenting.dev_mode, {})
 
   local presenting_autocmd_group_id = vim.api.nvim_create_augroup("PresentingAutoGroup", {})
@@ -41,15 +41,21 @@ Presenting.config = {
     -- The width of the slide buffer.
     width = 60,
   },
-  separator = {
-    -- Separators for different filetypes.
+
+  slide_separator = {
+    -- Slide separators for different filetypes.
     -- You can add your own or oberwrite existing ones.
     -- Note: separators are lua patterns, not regexes.
-    markdown = "^#+ ",
+    markdown = "^# ",
     org = "^*+ ",
     adoc = "^==+ ",
     asciidoctor = "^==+ ",
   },
+  step_separator = {
+    -- Step separators.
+    markdown = "^## ",
+  },
+
   -- Keep the separator, useful if you're parsing based on headings.
   -- If you want to parse on a non-heading separator, e.g. `---` set this to false.
   keep_separator = true,
@@ -57,13 +63,15 @@ Presenting.config = {
     -- These are local mappings for the open slide buffer.
     -- Disable existing keymaps by setting them to `nil`.
     -- Add your own keymaps as you desire.
-    ["n"] = function() Presenting.next() end,
-    ["p"] = function() Presenting.prev() end,
+    ["n"] = function() Presenting.next_step() end,
+    ["p"] = function() Presenting.prev_step() end,
     ["q"] = function() Presenting.quit() end,
     ["f"] = function() Presenting.first() end,
     ["l"] = function() Presenting.last() end,
-    ["<CR>"] = function() Presenting.next() end,
-    ["<BS>"] = function() Presenting.prev() end,
+    ["t"] = function() Presenting.top() end,
+    ["b"] = function() Presenting.bottom() end,
+    ["<CR>"] = function() Presenting.next_step() end,
+    ["<BS>"] = function() Presenting.prev_step() end,
   },
   -- A function that configures the slide buffer.
   -- If you want custom settings write your own function that accepts a buffer id as argument.
@@ -75,39 +83,32 @@ Presenting.config = {
 --- # Core functionality
 
 --- Toggle presenting mode on/off for the current buffer.
----@param separator string|nil
-Presenting.toggle = function(separator)
+Presenting.toggle = function(cmd_opts)
   if H.in_presenting_mode() then
     Presenting.quit()
   else
-    Presenting.start(separator)
+    Presenting.start(cmd_opts.fargs[1], cmd_opts.fargs[2])
   end
 end
 
 --- Start presenting the current buffer.
----@param separator string|nil Overwrite the default separator if specified.
-Presenting.start = function(separator)
+---@param slide_sep string|nil Overwrite the default slide_separator if specified.
+---@param step_sep string|nil Overwrite the default step_separator if specified.
+Presenting.start = function(slide_sep, step_sep)
   if H.in_presenting_mode() then
     vim.notify("Already presenting")
     return
   end
 
-  if type(separator) == "table" then
-    -- FIXME: why is separator a table when I don't pass anything?
-    -- into nil. I don't know why I get a table here when I don't pass anything.
-    -- print(vim.inspect(separator))
-    -- Workaround: turn not passed separator
-    separator = nil
-  end
-
   local filetype = vim.bo.filetype
-  separator = separator or Presenting.config.separator[filetype]
+  slide_sep = slide_sep or Presenting.config.slide_separator[filetype]
+  step_sep = step_sep or Presenting.config.step_separator[filetype]
 
-  if separator == nil then
+  if slide_sep == nil or step_sep == nil then
     vim.notify(
       "presenting.nvim does not support filetype "
         .. filetype
-        .. ". You can specify a separator manually: Presenting.start('---')"
+        .. ". You can specify slide and step separators manually: e.g. Presenting.start('---slide', '---step')"
     )
     return
   end
@@ -116,6 +117,7 @@ Presenting.start = function(separator)
     filetype = filetype,
     slides = {},
     slide = 1,
+    step = 1,
     n_slides = nil,
     slide_buf = nil,
     slide_win = nil,
@@ -128,7 +130,8 @@ Presenting.start = function(separator)
 
   -- content of slides
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-  Presenting._state.slides = H.parse_slides(lines, separator, Presenting.config.keep_separator)
+  Presenting._state.slides =
+    H.parse_slides(lines, slide_sep, step_sep, Presenting.config.keep_separator)
   Presenting._state.n_slides = #Presenting._state.slides
 
   H.create_slide_view(Presenting._state)
@@ -156,25 +159,68 @@ end
 
 --- Go to the next slide.
 --- By default this is mapped to `<CR>` and `n`.
-Presenting.next = function()
+Presenting.next_step = function()
   if not H.in_presenting_mode() then
     vim.notify("Not presenting. Call `PresentingStart` first.")
     return
   end
-  H.set_slide_content(
-    Presenting._state,
-    math.min(Presenting._state.slide + 1, Presenting._state.n_slides)
-  )
+
+  local num_steps = #Presenting._state.slides[Presenting._state.slide].steps
+  local next_step = Presenting._state.step + 1
+  local next_slide = Presenting._state.slide
+  if next_step > num_steps then
+    next_slide = math.min(next_slide + 1, Presenting._state.n_slides)
+    if next_slide ~= Presenting._state.slide then
+      next_step = 1
+    else
+      next_step = next_step - 1
+    end
+  end
+
+  H.set_slide_content(Presenting._state, next_slide, next_step)
 end
 
 --- Go to the previous slide.
 --- By default this is mapped to `<BS>` and `p`.
-Presenting.prev = function()
+Presenting.prev_step = function()
   if not H.in_presenting_mode() then
     vim.notify("Not presenting. Call `PresentingStart` first.")
     return
   end
-  H.set_slide_content(Presenting._state, math.max(Presenting._state.slide - 1, 1))
+
+  local next_step = Presenting._state.step - 1
+  local next_slide = Presenting._state.slide
+  if next_step < 1 then
+    next_slide = math.max(next_slide - 1, 1)
+    if next_slide ~= Presenting._state.slide then
+      next_step = #Presenting._state.slides[next_slide].steps
+    else
+      next_step = next_step + 1
+    end
+  end
+
+  H.set_slide_content(Presenting._state, next_slide, next_step)
+end
+
+-- Go to the beginning of the current slide.
+Presenting.top = function ()
+  if not H.in_presenting_mode() then
+    vim.notify("Not presenting. Call `PresentingStart` first.")
+    return
+  end
+
+  H.set_slide_content(Presenting._state, Presenting._state.slide, 1)
+end
+
+-- Go to the end of the current slide.
+Presenting.bottom = function ()
+  if not H.in_presenting_mode() then
+    vim.notify("Not presenting. Call `PresentingStart` first.")
+    return
+  end
+
+  local step = #Presenting._state.slides[Presenting._state.slide].steps
+  H.set_slide_content(Presenting._state, Presenting._state.slide, step)
 end
 
 --- Go to the first slide.
@@ -184,7 +230,7 @@ Presenting.first = function()
     vim.notify("Not presenting. Call `PresentingStart` first.")
     return
   end
-  H.set_slide_content(Presenting._state, 1)
+  H.set_slide_content(Presenting._state, 1, 1)
 end
 
 --- Go to the last slide.
@@ -194,7 +240,9 @@ Presenting.last = function()
     vim.notify("Not presenting. Call `PresentingStart` first.")
     return
   end
-  H.set_slide_content(Presenting._state, Presenting._state.n_slides)
+
+  local step = #Presenting._state.slides[Presenting._state.n_slides].steps
+  H.set_slide_content(Presenting._state, Presenting._state.n_slides, step)
 end
 
 ---Resize the slide window.
@@ -300,27 +348,51 @@ H.create_slide_view = function(state)
   Presenting.config.configure_slide_buffer(state.slide_buf)
   H.set_slide_keymaps(state.slide_buf, Presenting.config.keymaps)
 
-  H.set_slide_content(state, 1)
+  H.set_slide_content(state, 1, 1)
 end
 
 ---@param lines table
----@param separator string
+---@param slide_sep string
+---@param step_sep string
 ---@return table
 ---@private
-H.parse_slides = function(lines, separator, keep_separator)
-  -- TODO: isn't there a split() in lua that keeps the separator?
+H.parse_slides = function(lines, slide_sep, step_sep, keep_separator)
   local slides = {}
-  local slide = {}
+  local slide = {
+    steps = {},
+  }
+  local step = {}
+
   for _, line in pairs(lines) do
-    if line:match(separator) then
-      if #slide > 0 then table.insert(slides, table.concat(slide, "\n")) end
-      slide = {}
-      if keep_separator then table.insert(slide, line) end
+    if line:match(slide_sep) then
+      if #slide.steps > 0 then
+        table.insert(slide.steps, step)
+        table.insert(slides, slide) 
+      end
+      -- create new slide & step
+      slide = {
+        steps = {},
+      }
+      step = {}
+      if keep_separator then 
+        table.insert(step, line) 
+      end
+    elseif line:match(step_sep) then
+      if #step > 0 then
+        table.insert(slide.steps, step)
+        -- create new step
+        step = {}
+        if keep_separator then
+          table.insert(step, line) 
+        end
+      end
     else
-      table.insert(slide, line)
+      table.insert(step, line)
     end
   end
-  table.insert(slides, table.concat(slide, "\n"))
+
+  table.insert(slide.steps, step)
+  table.insert(slides, slide)
 
   return slides
 end
@@ -337,18 +409,20 @@ end
 
 ---@param state table
 ---@param slide integer
+---@param step integer
 ---@private
-H.set_slide_content = function(state, slide)
+H.set_slide_content = function(state, slide, step)
   local orig_modifiable = vim.api.nvim_buf_get_option(state.slide_buf, "modifiable")
   vim.api.nvim_buf_set_option(state.slide_buf, "modifiable", true)
   state.slide = slide
-  vim.api.nvim_buf_set_lines(
-    state.slide_buf,
-    0,
-    -1,
-    false,
-    vim.split(state.slides[state.slide], "\n")
-  )
+  state.step = step
+  local lines = {}
+  for i = 1, state.step do
+    for _, l in ipairs(state.slides[state.slide].steps[i]) do
+      lines[#lines + 1] = l
+    end
+  end
+  vim.api.nvim_buf_set_lines(state.slide_buf, 0, -1, false, lines)
   vim.api.nvim_buf_set_option(state.slide_buf, "modifiable", orig_modifiable)
 
   local footer_text = "presenting.nvim | " .. state.slide .. "/" .. state.n_slides
